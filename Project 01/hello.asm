d2821ff71c5622b40cbcb203595232afa876d3c8
AbtahiChowdhury <abtahichowdhury@gmail.com>
2020-02-18 08:49:04 -0500

    changes

--- Project 01/hello.asm
+++ Project 01/hello.asm
@@ -13,11 +13,11 @@
 _start: mov         eax, 4            ;system call number for write
         mov         ebx, 1            ;file handle 1 is stdout
         mov         ecx, message      ;address of string to output
-        mov         edx, 35           ;number of bytes (1 per character)
+        mov         edx, 25           ;number of bytes (1 per character)
         int 80h                       ;request an interrupt on libc using INT 80h
 exit:   mov         eax, 1            ;syscam call number for exit
         mov         ebx, 0            ;return 0 status on exit - 'No Errors'
         int 80h                       ;request an interrupt on libc using INT 80h
 
         section     .data
-message:db          "Abtahi Chowdhury, 23477449, MM1", 0Ah
+message:db          "Abtahi Chowdhury, MM1", 0Ah
